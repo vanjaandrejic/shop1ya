0ca8880f776891868fae287a89eca7438c6a4475
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-12-15 16:26:17 +0100

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./components/layout/Header";
 
 const App = () => {
-  return <div>Hello Clear Vite</div>;
+  return <Header/>;
 };
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h1>Shop1ya</h1>
+    </header>
+  );
+};
+
+export default Header;
