287a12e69c57b6291396afd57f455a6c86468e02
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-12-16 15:57:48 +0100

    react router starting setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Shop from "./components/Shop/Shop";
 
 const App = () => {
-  return <Layout />
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route index path="/" element={<Shop />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
